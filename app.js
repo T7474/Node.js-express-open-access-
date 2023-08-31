7d252e4b1109c7246a6f12ec92c2c2d4ee009304
T7474 <boatmala@hotmail.com>
2023-08-31 20:08:46 +0700

    Summary

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const PORT = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
+app.set("views","./src/views");
+app.set("views engine","ejs")
+
 app.get("/", (req, res) => {
 
-    res.send("hello borntoDevv");
+    res.render("index",{username: "boatevans"});
 
 })
 
-app.listen(port, ()=>{
+app.listen(PORT, ()=>{
     console.log("listening on port" );
 })
